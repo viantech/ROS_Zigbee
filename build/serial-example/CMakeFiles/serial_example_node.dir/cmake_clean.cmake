0828a06bc50578ebe38d7549898b39bb0762732f
Demo User <ubuntu@arm.localdomain>
2015-12-11 23:19:40 +0000

    Recv RFID posisition

---
+++ build/serial-example/CMakeFiles/serial_example_node.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/serial_example_node.dir/src/serial_example_node.cpp.o"
+  "CMakeFiles/serial_example_node.dir/src/uart.cpp.o"
+  "/home/ubuntu/catkin_ws/devel/lib/serial_example/serial_example_node.pdb"
+  "/home/ubuntu/catkin_ws/devel/lib/serial_example/serial_example_node"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/serial_example_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
